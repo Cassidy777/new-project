eccb31248349de9b75f3f7164316504ba4d1c2e7
Cassidy777 <Cassidy.take@gmail.com>
2024-10-29 15:24:18 +0900

    Initial commit

---
+++ front.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Hello() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    async function getMessage() {
+      try {
+        const url = "あなたのバックエンドデプロイ先URL";
+        const res = await axios.get(url);
+        setMessage(res.data.message);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getMessage();
+  }, []);
+
+  return (
+    <>
+      <h1>{message}</h1>
+    </>
+  );
+}
